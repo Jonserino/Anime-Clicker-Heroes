6c156f80ce935ddd5b281b1cb61039e6616d1105
Jonserino <jonas05@outlook.com>
2022-10-07 13:47:19 +0200

    A LOT
    I've made another hero, another upgrade system, kind of made the spawn and die functions better. More coming soon.
    
    Next will be to make the Monsters scale alongside the heroes.

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-monster_EL = document.querySelector("#monsters");
-gold_EL = document.querySelector("#gold");
-hero_EL = document.querySelector("#hero");
-hero_num_EL = document.querySelector("#heroNum");
-hero_hire_EL = document.querySelector("#cost");
+var monster_EL = document.querySelector("#monsters");
+var gold_EL = document.querySelector("#gold");
 
-container_EL = document.querySelector("#container");
-money_EL = document.querySelector("#money");
-heroes_EL = document.querySelector("#heroesID");
-images_EL = document.querySelector("#images");
+var cat_Knight_EL = document.querySelector("#catKnight");
+var cat_Knight_Num_EL = document.querySelector("#catKnightNum");
+var hire_Cat_Knight_EL = document.querySelector("#catKnightCost");
+
+var archer_EL = document.querySelector("#archer");
+var archer_Num_EL = document.querySelector("#archerNum");
+var hire_Archer_EL = document.querySelector("#archerCost");
+
+var container_EL = document.querySelector("#container");
+var money_EL = document.querySelector("#money");
+var heroes_EL = document.querySelector("#heroesID");
+var images_EL = document.querySelector("#images");
 
 
 let health = document.getElementById("health");
@@ -17,19 +22,20 @@ function random(max){
 }
 
 
-gold = 0;
-heroes = 0;
-seconds = 1000;
+var gold = 0;
 var drop = 6;
 var hp = 12;
 var maxHp = 12;
-var hire = 24;
-var atk = 4;
 
-function catknight(){
-    hire = 24;
-    atk = 4;
-}
+var archers = 0;
+var archerSeconds = 2000;
+var hireArcher = 36;
+var archerAtk = 6;
+
+var catKnights = 0;
+var catKnightSeconds = 1000;
+var hireCatKnight = 24;
+var catKnightAtk = 4;
 
 function slimes(){
     monster_EL.src="Pictures/Monsters/slimeMonster.png";
@@ -50,6 +56,7 @@ console.log(hp);
 console.log(random(2));
 
 function spawn(){
+    die()
     if (monster_EL.style.visibility="hidden"){
         monster_EL.style.visibility="visible";
     }
@@ -69,15 +76,18 @@ function spawn(){
 function monsterClick(){
     console.log(health.value);
     health.value -= 1;
-    die();
-    spawn();
 }
 
-function heroAtk(){
+function catKnightAtks(){
     console.log(health.value);
-    health.value -= atk;
-    die();
-    spawn();
+    health.value -= catKnightAtk;
+    console.log(catKnightAtk);
+}
+
+function archerAtks(){
+    console.log(health.value);
+    health.value -= archerAtk;
+    console.log(archerAtk);
 }
 
 function die(){
@@ -91,28 +101,54 @@ function die(){
     }
 }
 
-function hireHero(){
-    if (gold >= 10){
-        heroes++;
-        hero_num_EL.innerHTML = "Level: " + heroes;
-        hero_hire_EL.innerHTML = "Hire: " + hire;
-        gold -= hire;
-        seconds -= 10;
-        timer = setInterval(myTimer, seconds);
+function hireCatKnights(){
+    if (gold >= hireCatKnight){
+        catKnights++;
+        cat_Knight_Num_EL.innerHTML = "Level: " + catKnights;
+        gold -= hireCatKnight;
+        catKnightSeconds -= 10;
+        catKnightTimer = setInterval(myTimerCatKnight, catKnightSeconds);
+        hireCatKnight = Math.round(hireCatKnight * 1.30);
+        hire_Cat_Knight_EL.innerHTML = "Upgrade: " + hireCatKnight + " Gold";
+        if (catKnights >= 2){
+            catKnightAtk = Math.round(catKnightAtk * 1.40);
+        }    
+    }
+}
+
+function hireArchers(){
+    if (gold >= hireArcher){
+        archers++;
+        archer_Num_EL.innerHTML = "Level: " + archers;
+        gold -= hireArcher;
+        archerSeconds -= 10;
+        archerTimer = setInterval(myTimerArcher, archerSeconds);
+        hireArcher = Math.round(hireArcher * 1.30);
+        hire_Archer_EL.innerHTML = "Upgrade: " + hireArcher + " Gold";
+        if (archers >= 2){
+            archerAtk = Math.round(archerAtk * 1.40);
+        }    
     }
 }
 
 
-
-
-
 monster_EL.addEventListener("click", monsterClick); // Når vi klikker på monster
-hero_EL.addEventListener("click", hireHero);
+cat_Knight_EL.addEventListener("click", hireCatKnights);
+archer_EL.addEventListener("click", hireArchers);
 
-var timer = setInterval(myTimer, seconds); // Kjører funksjonen mytimer 1 gang i sekundet
-function myTimer(){
-    if (heroes >= 1){
-        heroAtk();
+var catKnightTimer = setInterval(myTimerCatKnight, catKnightSeconds); // Kjører funksjonen mytimer 1 gang i sekundet
+var archerTimer = setInterval(myTimerArcher, archerSeconds); // Kjører funksjonen mytimer 1 gang i sekundet
+var spawn_check = setInterval(spawn, 10)
+
+function myTimerCatKnight(){
+    if (catKnights >= 1){
+        catKnightAtks();
     }
     gold_EL.innerHTML = gold;
+}
+function myTimerArcher(){
+    if (archers >= 1){
+        archerAtks();
+    }
+    gold_EL.innerHTMl = gold;
 }
